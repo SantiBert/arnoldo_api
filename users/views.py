813fd6328fae1f488dac52eecd3386c27756bf67
SantiBert <santibertero34@gmail.com>
2023-04-12 04:22:57 -0300

    login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.views import GenericAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth import authenticate
+
+from .models import CustomUser
+from .serializer import CustomUserSerializer, CustomRegisterSerializer, CustomLoginSerializer
+
+
+class CustomUserView(GenericAPIView):
+    
+    serializer_class = CustomUserSerializer
+    
+    def get(self, request):
+        users = CustomUser.objects.filter(is_active=True)
+        serializer = CustomUserSerializer(users, many=True)
+        return Response({"data": serializer.data}, status=200)
+
+class CustomRegisterView(GenericAPIView):
+    
+    serializer_class = CustomRegisterSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"errors": serializer.errors}, status=400)
+        
+        try:
+            user = serializer.save()
+            
+            token = Token.objects.create(user=user)
+            json = serializer.data
+            json['token'] = token.key
+            return Response(json, status=200)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
+        
+        
+class LoginView(GenericAPIView):
+    serializer_class = CustomLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if not serializer.is_valid():
+            return Response({"errors": serializer.errors}, status=400)
+        try:
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(request=request, username=email, password=password)
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key})
+            else:
+                return Response({'error': 'Usuario o contraseña incorrectos'}, status=401)
+        except Exception as e:
+            return Response({"error": str(e)}, status=500)
